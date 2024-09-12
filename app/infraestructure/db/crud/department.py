649a1f70971971d94ad1c6409384e306ffe8cf37
smoke <jhon.jaramilloe@udea.edu.co>
2024-09-12 14:33:07 -0500

    se elimina school y deparment

--- app/infraestructure/db/crud/department.py
+++
@@ -1,9 +0,0 @@
-from app.infraestructure.db.crud.base import CRUDBase
-from app.infraestructure.db.models import Department
-from app.schemas.department import DepartmentCreate, DepartmentUpdate
-
-
-class DepartmentCrud(CRUDBase[Department, DepartmentCreate, DepartmentUpdate]):
-    ...
-    
-department_crud = DepartmentCrud(Department)
