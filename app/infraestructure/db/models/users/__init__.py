1c096e58eb0c3008c9161d1f2219b7b607ce75a5
alwos19 <jhon.jaramilloe@udea.edu.co>
2024-09-16 16:32:12 -0500

    montando infraestructura de usuarios

--- app/infraestructure/db/models/users/__init__.py
+++ app/infraestructure/db/models/users/__init__.py
@@ -3,3 +3,6 @@ from .rol import Rol
 from .user_rol import UserRol
 from .administrative_type import AdministrativeType
 from .administrative import Administrative
+from .professor_type import ProfessorType
+from .professor import Porfesor
+from .student import Student
--- app/infraestructure/db/models/users/administrative_type.py
+++ app/infraestructure/db/models/users/administrative_type.py
@@ -1,10 +1,6 @@
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+
 from app.infraestructure.db.utils.base_model import BaseModel
 
 class AdministrativeType(BaseModel):
     name = Column (String(100), nullable=False)
-
-# relations
-
-# administrative = relationship("Administrative", back_populates="administrative_type")
