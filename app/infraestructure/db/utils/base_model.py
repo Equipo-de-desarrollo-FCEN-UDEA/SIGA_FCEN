0021d8cfa9de7999f7f3379bd5d49036bb09d195
alwos19 <jhon.jaramilloe@udea.edu.co>
2024-09-17 13:06:31 -0500

    resolviendo conflictos

--- app/infraestructure/db/utils/base_model.py
+++ app/infraestructure/db/utils/base_model.py
@@ -5,8 +5,4 @@ from uuid import uuid4
 
 class BaseModel(Base):
     __abstract__ = True
-<<<<<<< HEAD
     id = Column(Uuid, default=uuid4 ,primary_key=True)
-=======
-    id = Column(Uuid, default=uuid4 ,primary_key=True)
->>>>>>> develop
