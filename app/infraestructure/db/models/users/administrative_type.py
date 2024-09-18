33d47b5e43f23b2bafe025c454a1355e2f9c4a43
Esteban Durango <estebandurangov>
2024-09-18 10:52:54 -0500

    update relationships

--- app/infraestructure/db/models/users/administrative_type.py
+++ app/infraestructure/db/models/users/administrative_type.py
@@ -1,6 +1,10 @@
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 from app.infraestructure.db.utils.base_model import BaseModel
 
 class AdministrativeType(BaseModel):
     name = Column (String(100), nullable=False)
+
+    # relations
+    administratives = relationship("Administrative", back_populates="administrative_type")
