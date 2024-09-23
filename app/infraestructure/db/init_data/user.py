179cc8baef58c148d19ef20264162ae2331a1b20
alwos19 <jhon.jaramilloe@udea.edu.co>
2024-09-23 15:20:51 -0500

    configuracion del inicio de datis

--- app/infraestructure/db/init_data/user.py
+++ app/infraestructure/db/init_data/user.py
@@ -4,8 +4,16 @@ from app.core.config import get_settings
 
 settings = get_settings()
 
-init_users: List[user.UserCreate] =[
+init_users: List[user.UserCreate] = [
     user.UserCreate(
+    name = "pedro",
+    last_name = "perez",
+    email = "pedro@local.com",
+    identification_type = "cedula_ciudadania",
+    identification_number="123456789",
+    phone="123456789",
+    hashed_password= "123456789",    
+    is_active = True
        
     )
 ]
