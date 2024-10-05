e4dbdb412a1c2e61419665f983c0e37ab07c7913
Esteban Durango <estebandurangov>
2024-10-05 10:34:58 -0500

    se agrega la configuracion de observadores para infraestructura

--- app/services/application/user_application.py
+++ app/services/application/user_application.py
@@ -3,3 +3,8 @@ from app.schemas.application.user_application import UserApplicationCreate, User
 from app.protocols.db.models.application.user_application import UserApplication
 from app.protocols.db.crud.application.user_application import CRUDUserApplicationProtocol
 
+class UserApplicationService(ServiceBase[UserApplication, UserApplicationCreate, UserApplicationUpdate, CRUDUserApplicationProtocol]):
+    pass
+
+user_application_svc = UserApplicationService()
+
