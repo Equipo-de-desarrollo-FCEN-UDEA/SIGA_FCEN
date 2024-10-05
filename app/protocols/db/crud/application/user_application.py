03d37012184fec06e258e7b646fae18714d2a308
Esteban Durango <estebandurangov>
2024-10-05 10:30:17 -0500

    se agregan los servicios de las aplicaciones

--- app/protocols/db/crud/application/user_application.py
+++ app/protocols/db/crud/application/user_application.py
@@ -2,5 +2,5 @@ from app.protocols.db.crud.base import CRUDProtocol
 from app.protocols.db.models.application.user_application import UserApplication
 from app.schemas.application.user_application import UserApplicationCreate, UserApplicationUpdate
 
-class CRUDRUserApplicationProtocol(CRUDProtocol[UserApplication, UserApplicationCreate, UserApplicationUpdate]):
+class CRUDUserApplicationProtocol(CRUDProtocol[UserApplication, UserApplicationCreate, UserApplicationUpdate]):
     ...
---
+++ app/services/application/user_application.py
@@ -0,0 +1,5 @@
+from app.services.base import ServiceBase
+from app.schemas.application.user_application import UserApplicationCreate, UserApplicationUpdate
+from app.protocols.db.models.application.user_application import UserApplication
+from app.protocols.db.crud.application.user_application import CRUDUserApplicationProtocol
+
