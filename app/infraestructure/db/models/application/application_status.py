b6ca3d7c5ef8214605cdaef2576b43f7397a8b93
jeronimoloaiza <jeronimo.loaiza@udea.edu.co>
2024-10-11 11:59:28 -0500

    Invirtiendo la relacion de application_status con user_application

--- app/infraestructure/db/models/application/application_status.py
+++ app/infraestructure/db/models/application/application_status.py
@@ -1,11 +1,12 @@
-from sqlalchemy import Column, Enum
+from sqlalchemy import Column, Uuid, Enum, ForeignKey
 from sqlalchemy.orm import relationship
 
 from app.infraestructure.db.utils.base_model import BaseModel
 from app.protocols.db.models.application.application_status import ApplicationStatusType
 
 class ApplicationStatus(BaseModel):
+    user_application_id = Column(Uuid, ForeignKey("user_application.id"), nullable=False)
     name = Column(Enum(ApplicationStatusType), nullable=False)
 
     # relations
-    user_applications = relationship("UserApplication", back_populates="application_status")
+    user_application = relationship("ApplicationStatus", back_populates="application_statuses")
--- app/infraestructure/db/models/application/user_application.py
+++ app/infraestructure/db/models/application/user_application.py
@@ -6,10 +6,9 @@ from app.infraestructure.db.utils.base_model import BaseModel
 class UserApplication(BaseModel):
     user_id = Column(Uuid, ForeignKey("user.id"), nullable=False)
     application_id = Column(Uuid, ForeignKey("application.id"), nullable=False)
-    application_status_id = Column(Uuid, ForeignKey("application_status.id"),nullable=False)
 
     # relations
     user = relationship("User", back_populates="user_applications")
     application = relationship("Application", back_populates="user_applications")
-    application_status = relationship("ApplicationStatus", back_populates="user_applications")
+    application_statuses = relationship("ApplicationStatus", back_populates="user_application")
     user_application_academic_units = relationship("UserApplicationAcademicUnit", back_populates="user_application")
