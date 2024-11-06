787bab9cdb904e2ad1b9d5dec1cae527c12f6425
Esteban Durango <esteban.durangov@udea.edu.co>
2024-11-06 18:36:51 -0500

    se solucionan algunos errores en policies

--- app/infraestructure/policies/voting/voting.py
+++ app/infraestructure/policies/voting/voting.py
@@ -2,10 +2,12 @@ from uuid import UUID
 
 from app.schemas.voting.voting import VotingResponse
 from app.services.application.type.mobility import mobility_svc
+from app.services.voting.voting_info import voting_info_svc
 
 async def get_application_in_mongo(*,
     voting,
     db) -> dict:
+    voting_id = voting.id
     user_application_id = voting.user_application_id
     application_type = voting.user_application.application.name
     
@@ -15,5 +17,8 @@ async def get_application_in_mongo(*,
     if application_type == 'MOVILIDAD':
         mobility = {"mobility": await mobility_svc.get(id=user_application_id, db=db)}
         if mobility is None:
-            raise ValueError(f"La solicitud de movilidad con id {user_application_id} no existe")        
-        return voting['user_application'] | mobility
+            raise ValueError(f"La solicitud de movilidad con id {user_application_id} no existe")
+        info_voting = {"info_voting": await voting_info_svc.get(id=voting_id, db=db)}
+        voting['user_application'].update(mobility)
+        voting.update(info_voting)
+        return voting
